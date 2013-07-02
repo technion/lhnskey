666395ac2fa9d74a1be6ca26cb2cd1316b2eb42a
Technion <technion@lolware.net>
2013-07-02 07:57:35 -0400

    Added main file.

---
+++ lhnskey.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+#include "md5.h"
+#define MD5_LEN 16
+#define LHNSTR "RedHook LHN 4.1 SKEY 20030101 - Kelly"
+#define KEYLEN 17
+
+void printstr(unsigned char *input, const size_t len);
+
+int main() {
+
+	MD5_CTX ctx;
+	unsigned char result[MD5_LEN];
+	char mac[20];
+
+	fgets(mac, sizeof(mac), stdin);
+	if (strlen(mac) != (KEYLEN+1))
+	{
+		printf("Invalid key input %d\n", strlen(mac));
+		exit(1);
+	}
+	if (mac[KEYLEN] != '\n')
+	{
+		printf("Invalid key input\n");
+		exit(1);
+	}
+	mac[KEYLEN] = '\0';
+
+	/* Root password */
+
+	MD5_Init(&ctx);
+	MD5_Update(&ctx, LHNSTR, strlen(LHNSTR));
+	MD5_Update(&ctx, mac, KEYLEN);
+	MD5_Final(result, &ctx);
+	printstr(result, 5);
+
+	/* S/Key secret */
+
+	MD5_Init(&ctx);
+	MD5_Update(&ctx, LHNSTR, strlen(LHNSTR));
+	MD5_Final(result, &ctx);
+	printstr(result, MD5_LEN);
+
+	return 0;
+
+}
+
+void printstr(unsigned char *input, const size_t len) {
+	int i;
+
+	for (i=0; i<len; i++)
+	{
+		printf("%02x", (unsigned int) input[i]);
+	}
+	printf("\n");
+}
+
